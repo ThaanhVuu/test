48549fe64f4fce9e5bb879fad5edf79656ef61f4
thaanhvuu <thanhvu7603@gmail.com>
2025-11-27 17:52:43 +0700

    build: ?

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module dhkthn.p2p {
     requires MaterialFX;
     requires org.kordamp.bootstrapfx.core;
     requires static lombok;
+    requires javafx.controls;
+    requires javafx.fxml;
+
     opens dhkthn.p2p to javafx.fxml;
     exports dhkthn.p2p;
 }
