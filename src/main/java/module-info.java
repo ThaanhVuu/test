bd93972c0e571bda2d77d9b13423e6d4d374eb8e
thaanhvuu <thanhvu7603@gmail.com>
2025-11-27 17:59:46 +0700

    build: refactor module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,19 @@
 module dhkthn.p2p {
-    requires com.google.gson;
-    requires MaterialFX;
-    requires org.kordamp.bootstrapfx.core;
-    requires static lombok;
+    // JavaFX
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Third-party libraries
+    requires com.google.gson;
+    requires MaterialFX;
+    requires org.kordamp.bootstrapfx.core;
+
+    // Lombok (chỉ cần compile-time)
+    requires static lombok;
+
+    // Mở package cho FXMLLoader (bắt buộc nếu dùng FXML)
     opens dhkthn.p2p to javafx.fxml;
+
+    // Export để các class trong package này có thể được truy cập
     exports dhkthn.p2p;
 }
